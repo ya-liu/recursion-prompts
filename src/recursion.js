56e71e70e115a62c8ac7897f4e36a0b85da27817
Ya Liu <sarahl0216@gmail.com>
2021-07-18 15:49:53 -0700

    Question 20

--- src/recursion.js
+++ src/recursion.js
@@ -231,6 +231,13 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  return str1[0] === str2[0];
+
+  return compareStr(str1.slice(1), str2.slice(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
@@ -260,6 +267,15 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  var currentIndex = array.indexOf(value);
+
+  if (currentIndex === -1 || array.length === 0) {
+    return 0;
+  }
+
+  if (currentIndex >= 0) {
+    return countOccurrence(array.slice(currentIndex + 1), value) + 1;
+  }
 };
 
 // 21. Write a recursive version of map.
